5336c7bf73e05b04e95d02956574f45d9783bb79
Alex Petrovski <a.petrovsky@outlook.com>
2020-02-12 18:48:22 +0200

    167

---
+++ Leetcode/Array/Array/T167.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T167
+    {
+        [TestCase(new [] {2,7,11,15}, 9, new [] {1, 2})]
+        [TestCase(new [] {3, 2, 4}, 6, new []{2, 3})]
+        [TestCase(new [] {3, 3}, 6, new []{1, 2})]
+        public void Test(int[] array, int target, int[] result)
+        {
+            var map = new Dictionary<int, int>();
+
+            var r = new int[2];
+            for (int i = 0; i < array.Length; i++)
+            {
+                var el = array[i];
+                if (map.ContainsKey(target - el))
+                {
+                    if (map[target - el] < i + 1)
+                    {
+                        r[0] = map[target - el];
+                        r[1] = i + 1;
+                    }
+                    else
+                    {
+                        r[0] = i + 1;
+                        r[1] = map[target - el];
+                    }
+                    
+                    break;
+                }
+                else
+                {
+                    map[el] = i + 1;
+                }
+            }
+
+            r.Should().Equal(result);
+        }
+    }
+}
---
+++ Leetcode/Array/Array/T268.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T268
+    {
+        
+        [TestCase(new [] {3,0,1}, 2)]
+        [TestCase(new [] {9,6,4,2,3,5,7,0,1}, 8)]
+        public void Test(int[] nums, int result)
+        {
+            var current = nums.Length;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                current ^= i ^ nums[i];
+            }
+
+            current.Should().Be(result);
+        }
+        
+        [TestCase(new [] {3,0,1}, 2)]
+        [TestCase(new [] {9,6,4,2,3,5,7,0,1}, 8)]
+        public void TestGauss(int[] nums, int result)
+        {
+            var exptectedSum = nums.Length * (nums.Length + 1) / 2;
+            var actualResult = exptectedSum - nums.Sum();
+
+            actualResult.Should().Be(result);
+        }
+    }
+}
