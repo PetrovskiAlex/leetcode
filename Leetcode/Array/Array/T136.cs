66dda7279ac26cc5e7339eebf00a0f4dde6ff6ff
Alex Petrovski <a.petrovsky@outlook.com>
2020-02-12 18:52:53 +0200

    136

---
+++ Leetcode/Array/Array/T136.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T136
+    {
+        [TestCase(new []{2,2,1}, 1)]
+        [TestCase(new []{4,1,2,1,2}, 4)]
+        public void Test(int[] nums, int result)
+        {
+            var first = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                first ^= nums[i];
+            }
+
+            first.Should().Be(result);
+        }
+    }
+}
