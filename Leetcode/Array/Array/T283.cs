8f3d71d1d2b2680c9ad72b96a467426fc637cb5d
Alex Petrovski <a.petrovsky@outlook.com>
2020-02-14 17:45:32 +0200

    283 - processing

---
+++ Leetcode/Array/Array/T283.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T283
+    {
+        [TestCase(new [] {0,1,0,3,12}, new [] {1,3,12,0,0})]
+        public void Test(int[] input, int[] output)
+        {
+            var j = input.Length;
+            for (var i = 0; i < j; i++)
+            {
+                if (input[i] == 0)
+                {
+                    while (j > i && input[j - 1] == 0)
+                    {
+                        j--;
+                    }
+
+                    if (i >= j)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        input[i] = input[j - 1];
+                        input[j - 1] = 0;
+                    }
+                }
+            }
+
+            input.Should().Equal(output);
+        }
+    }
+}
