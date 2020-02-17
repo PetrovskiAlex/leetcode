ae5533aa4e8ea338d705b2054bf8f99f34999140
Alex Petrovski <a.petrovsky@outlook.com>
2020-02-17 21:41:00 +0200

    283

--- Leetcode/Array/Array/T283.cs
+++ Leetcode/Array/Array/T283.cs
@@ -9,39 +9,22 @@ namespace Array
         [TestCase(new [] {0, 0, 0, 0}, new [] {0, 0, 0, 0})]
         [TestCase(new [] {0, 0, 0, 0, 1}, new [] {1, 0, 0, 0, 0})]
         [TestCase(new [] {0, 0, -27}, new [] {-27, 0, 0})]
+        [TestCase(new [] {1, 1, 0}, new [] {1, 1, 0})]
         public void Test(int[] input, int[] output)
         {
+            var currentZero = 0;
             for (var i = 0; i < input.Length; i++)
             {
-                if (input[i] == 0)
+                if (input[i] != 0)
                 {
-                    var nonZeroIndex = FindNonZeroIndex(input, i + 1);
-                    if (nonZeroIndex > -1)
-                    {
-                        input[i] = input[nonZeroIndex];
-                        input[nonZeroIndex] = 0;    
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    var temp = input[currentZero]; 
+                    input[currentZero] = input[i];
+                    input[i] = temp;
+                    currentZero++;
                 }
             }
 
             input.Should().Equal(output);
-        }
-
-        private int FindNonZeroIndex(int[] array, int startIndex)
-        {
-            for (var i = startIndex; i < array.Length; i++)
-            {
-                if (array[i] != 0)
-                {
-                    return i;
-                }
-            }
-
-            return -1;
         }
     }
 }
