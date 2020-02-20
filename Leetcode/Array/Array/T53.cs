7505d6b58fcdb827a6646da78c2a4b0d0ac91286
Alex Petrovski <a.petrovsky@outlook.com>
2020-02-20 23:05:18 +0200

    35 working

---
+++ Leetcode/Array/Array/T53.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T53
+    {
+        [TestCase(new []{-2,1,-3,4,-1,2,1,-5,4}, 6)]
+        public void Test(int[] nums, int result)
+        {
+            var sum = 0;
+
+            foreach (var num in nums)
+            {
+                sum = Math.Max(sum, sum + num);
+                if (sum <= 0)
+                {
+                    sum = num;
+                }
+            }
+
+            sum.Should().Be(result);
+        }
+    }
+}
