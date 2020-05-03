89332726b7173252ca247ffc49b157907fe12295
Alex Petrovski <a.petrovsky@outlook.com>
2020-05-03 12:29:34 +0300

    238

---
+++ Leetcode/Array/Array/T238.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T238
+    {
+        [TestCase(new[] {1, 2, 3, 4}, new []{24,12,8,6})]
+        [TestCase(new[] {1,2,3,0,5,0}, new []{0,0,0,0,0,0})]
+        [TestCase(new[] {1,2,3,0,5}, new []{0,0,0,30,0})]
+        public void ProductExceptSelfTest(int[] nums, int[] output)
+        {
+            var result = GetProduct(nums);
+
+            result.Should().Equal(output);
+        }
+
+        private static int[] GetProduct(int[] nums)
+        {
+            var length = nums.Length;
+
+            var result = new int[length];
+
+            result[0] = 1;
+            for (var i = 1; i < length; i++) {
+
+                result[i] = nums[i - 1] * result[i - 1];
+            }
+
+            var x = 1;
+            for (var i = length - 1; i >= 0; i--)
+            {
+                result[i] = result[i] * x;
+                x *= nums[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Leetcode/Array/Array/T46.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T46
+    {
+        [TestCase(new []{1, 2, 3})]
+        public void PermuteTest(int[] nums) {
+            var result = new List<List<int>>();
+
+            var permute = Permute(nums);
+
+            result.Count.Should().Be(Factorial(nums.Length));
+        }
+
+        public List<List<int>> Permute(int[] nums) {
+            var list = new List<List<int>>();
+            // Arrays.sort(nums); // not necessary
+            Backtrack(list, new List<int>(), nums);
+            return list;
+        }
+
+        private void Backtrack(List<List<int>> list, List<int> tempList, int [] nums){
+            if(tempList.Count == nums.Length){
+                list.Add(new List<int>(tempList));
+            } else{
+                for(int i = 0; i < nums.Length; i++){ 
+                    if(tempList.Contains(nums[i])) continue; // element already exists, skip
+                    tempList.Add(nums[i]);
+                    Backtrack(list, tempList, nums);
+                    tempList.Remove(tempList.Count - 1);
+                }
+            }
+        } 
+        
+        private int Factorial(int n)
+        {
+            if (n == 1) return 1;
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
