986d1d9f6e15c3a9d70b81c658f8f380cbe5d0d8
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-05-06 20:45:19 +0300

    T977

---
+++ Leetcode/Array/Array/T977.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Array
+{
+    public class T977
+    {
+        [TestCase(new []{-4,-1,0,3,10}, new []{0,1,9,16,100})]
+        public void Test(int[] input, int[] output)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                input[i] = input[i] * input[i];
+            }
+            
+            System.Array.Sort(input);
+        }
+    }
+}
