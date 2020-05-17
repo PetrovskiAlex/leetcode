82e2911389f1b84a51d6dd97918c3b44877761bf
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-05-17 17:19:24 +0300

    T33

--- Leetcode/Array/Array/T33.cs
+++ Leetcode/Array/Array/T33.cs
@@ -11,32 +11,36 @@ namespace Array
         [TestCase(new [] {4,5,6,7,0,1,2}, 0, 4)]
         [TestCase(new [] {4,5,6,7,0,1,2}, 3, -1)]
         [TestCase(new [] {4,5,6,7,0,1,2}, 5, 1)]
+        [TestCase(new [] {4,5,6,7,8,0,1,2}, 0, 5)]
+        [TestCase(new [] {4,5,6,7,8,9,1}, 1, 6)]
+        [TestCase(new [] {4,5,6,7,8,1,2}, 1, 5)]
         [TestCase(new [] {4,5,6,7,0,1,2}, 4, 0)]
         public void Test(int[] nums, int target, int expected)
         {
-            var result = -1;
-
-            result = Search(nums, 0, nums.Length, target);
-
+            var result = Search(nums, target);
             result.Should().Be(expected);
         }
 
-        private int Search(int[] nums, int low, int high, int target)
+        private int Search(int[] nums, int target)
         {
-            if (low > high || low >= nums.Length) return -1;
-            if (low == high && nums[low] == target) return low;
-            if (nums[low] == target) return low;
-            if (high < nums.Length && nums[high] == target) return high;
+            var low = 0;
+            var high = nums.Length - 1;
+            
+            while (low < high)
+            {
+                if (nums[low] == target) return low;
+                if (nums[high] == target) return high;
 
-            var middle = (low + high) / 2;
-            if (nums[middle] == target) return middle;
+                var middle = (low + high) / 2;
+                if (nums[middle] == target) return middle;
 
-            var result = Search(nums, low, middle - 1, target);
-            if (result > -1) return result;
+                if (nums[middle] > nums[middle - 1])
+                {
+                    
+                }
+            }
 
-            result = Search(nums, middle + 1, high, target);
-
-            return result;
+            return -1;
         }
     }
 }
---
+++ Leetcode/Array/Array/T448.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T448
+    {
+        /*
+         * 448. Find All Numbers Disappeared in an Array
+         */
+        [TestCase(new []{4,3,2,7,8,2,3,1}, new [] {5,6})]
+        [TestCase(new []{1,1,1}, new [] {2,3})]
+        public void Test(int[] nums, int[] output)
+        {
+            if (!nums.Any()) return;
+            
+            var result = new List<int>();
+
+            
+            result.Should().BeEquivalentTo(output);
+        }
+    }
+}
