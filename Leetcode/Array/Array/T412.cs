db0c7b5e2d22e636be992bd9f8ddd6e6e50f9562
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-05-21 21:55:54 +0300

    T412

---
+++ Leetcode/Array/Array/T412.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array
+{
+    public class T412
+    {
+        public void Test()
+        {
+            var result = new List<string>();
+        
+            for(var i = 1; i <= 15; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0){
+                    result.Add("FizzBuzz");
+                }
+                else if(i % 3 == 0){
+                    result.Add("Fizz");
+                }
+                else if(i % 5 == 0){
+                    result.Add("Buzz");
+                }
+                else{
+                    result.Add(i.ToString());
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
