89877108593b03b2f98de0c562882ce41b2efdb1
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-05-21 22:01:19 +0300

    T344

---
+++ Leetcode/Array/Array/T344.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T344
+    {
+        [TestCase(new []{'h','e','l','l','o'}, new [] {'o','l','l','e','h'})]
+        public void Test(char[] s, char[] result)
+        {
+            var low = 0;
+            var high = s.Length - 1;
+
+            while (low < high)
+            {
+                var temp = s[low];
+                s[low] = s[high];
+                s[high] = temp;
+
+                low++;
+                high--;
+            }
+
+            s.Should().BeEquivalentTo(result);
+        }
+    }
+}
