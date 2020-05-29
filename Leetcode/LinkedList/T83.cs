32f5683d5fe6666942e6423e1ee8052821488ab6
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-05-29 21:47:36 +0300

    T83

---
+++ Leetcode/LinkedList/T83.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LinkedList
+{
+    /*
+     * 83. Remove Duplicates from Sorted List
+     */
+    public class T83 : BaseLinkedListTest
+    {
+        [TestCase(new [] {1,1,2}, new [] {1,2})]
+        [TestCase(new [] {1,1,1,2}, new [] {1,2})]
+        [TestCase(new [] {1,1,1,2,3,3}, new [] {1,2,3})]
+        public void Test(int[] input, int[] output)
+        {
+            var head = CreateLinkedList(input);
+            var copyHead = head;
+
+            while (head.next != null)
+            {
+                if (head.val == head.next.val)
+                {
+                    head.next = head.next?.next;
+                }
+                else
+                {
+                    head = head.next;
+                }
+            }
+            
+            ToArray(copyHead).Should().BeEquivalentTo(output);
+        }
+    }
+}
