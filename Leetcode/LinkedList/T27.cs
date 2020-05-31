1360464a09fbed047fab8aad167911e80b5a0b99
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-05-31 20:40:14 +0300

    T27

---
+++ Leetcode/LinkedList/T27.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LinkedList
+{
+    public class T27 : BaseLinkedListTest
+    {
+        [TestCase(new [] {3,2,2,3}, new [] {2,2}, 3)]
+        public void Test(int[] input, int[] output, int target)
+        {
+            var node = CreateLinkedList(input);
+            var empty = new ListNode(-1, node);
+            node = empty;
+
+            var copyHead = empty;
+
+            while (node.next != null)
+            {
+                if (node.next.val == target)
+                {
+                    node.next = node.next?.next;
+                }
+                else
+                {
+                    node = node.next;    
+                }
+            }
+
+            ToArray(copyHead.next).Should().BeEquivalentTo(output);
+        }
+    }
+}
