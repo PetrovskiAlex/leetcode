9a49999d5ee4ae35b9b7d33780637a6946e01be9
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-06-03 22:41:01 +0300

    T387

---
+++ Leetcode/Array/Array/T387.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T387
+    {
+        [TestCase("leetcode", 0)]
+        [TestCase("loveleetcode", 2)]
+        [TestCase("cc", -1)]
+        public void Test(string s, int index)
+        {
+            var array = new int [26];
+            
+            foreach (var ch in s)
+            {
+                array[ch - 'a']++;
+            }
+
+            var result = -1;
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (array[s[i] - 'a'] == 1)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            result.Should().Be(index);
+        }
+    }
+}
