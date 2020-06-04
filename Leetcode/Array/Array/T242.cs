8439a02fe15d56f632d9fa8ab60517c7c2b393e3
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-06-04 22:14:25 +0300

    T242

---
+++ Leetcode/Array/Array/T242.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T242
+    {
+        [TestCase("anagramz", "nagaramz", true)]
+        [TestCase("rat", "car", false)]
+        public void Test(string s, string t, bool output)
+        {
+            var array = new int[26];
+            foreach (var c in s)
+            {
+                array[c - 'a']++;
+            }
+
+            foreach (var c in t)
+            {
+                array[c - 'a']--;
+            }
+
+            var result = array.All(a => a == 0);
+
+            result.Should().Be(output);
+        }
+    }
+}
