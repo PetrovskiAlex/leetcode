100b658af5535deb4d4ace2e4d336baacee6ab16
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-06-04 22:35:18 +0300

    T49

---
+++ Leetcode/Array/Array/T49.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    /*
+     * 49. Group Anagrams
+     */
+    public class T49
+    {
+        [Test]
+        public void Test()
+        {
+            var array = new[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+            var output = new List<IList<string>>
+            {
+                new List<string>{"ate","eat","tea"},
+                new List<string>{"nat","tan"},
+                new List<string>{"bat"}
+            };
+            
+            var mapResult = new Dictionary<string, IList<string>>();
+
+            foreach (var s in array)
+            {
+                var chars = s.ToCharArray();
+                System.Array.Sort(chars);
+                var newString = new string(chars);
+                
+                if (mapResult.ContainsKey(newString))
+                {
+                    mapResult[newString].Add(s);
+                }
+                else
+                {
+                    mapResult[newString] = new List<string>{s};
+                }
+            }
+
+            var result = mapResult.Select(kv => kv.Value).ToList();
+
+            result.Should().BeEquivalentTo(output);
+        }
+    }
+}
