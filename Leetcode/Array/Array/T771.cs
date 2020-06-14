cc0f9bf079bd27c7458fb9482ae173185319a615
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-06-14 22:27:41 +0300

    T771

---
+++ Leetcode/Array/Array/T771.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Array
+{
+    public class T771
+    {
+        [TestCase("aA", "aAAbbbb", 3)]
+        [TestCase("z", "ZZ", 0)]
+        public void Test(string j, string s, int output)
+        {
+            var map = new Dictionary<char, int>();
+            foreach (var c in j)
+            {
+                map[c] = 0;
+            }
+
+            var result = s.Count(c => map.ContainsKey(c));
+
+            result.Should().Be(output);
+        }
+    }
+}
