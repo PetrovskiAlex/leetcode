544bf1a1102dc2ab473d37b353759791d6cfb10f
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-06-19 22:39:46 +0300

    T90

---
+++ Leetcode/Backtracking/T90.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Backtracking
+{
+    public class T90
+    {
+        [Test]
+        public void Test()
+        {
+            var numbers = new [] { 1,2,2 };
+            var result = new List<List<int>>();
+            Array.Sort(numbers);
+
+            Dfs(0, numbers, new List<int>(), result);
+
+            result = result.OrderBy(r => r.Count).ToList();
+            
+            result.Count.Should().Be(6);
+        }
+
+        private void Dfs(int start, int[] nums, List<int> temp, List<List<int>> result)
+        {
+            result.Add(new List<int>(temp));
+            for (var i = start; i < nums.Length; i++)
+            {
+                if (i == start || nums[i] != nums[i - 1])
+                {
+                    temp.Add(nums[i]);
+                    Dfs(i + 1, nums, temp, result);
+                    temp.RemoveAt(temp.Count - 1);
+                }
+            }
+        }
+    }
+}
