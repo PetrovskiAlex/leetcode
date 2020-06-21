319b8f2adaaf41e93b2d6c0edac2fbaa8403d054
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-06-21 22:16:01 +0300

    T47

---
+++ Leetcode/Backtracking/T47.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Backtracking
+{
+    public class T47
+    {
+        [TestCase(new [] {1,1,2})]
+        public void Test(int[] nums)
+        {
+            var result = new List<IList<int>>();
+
+            Dfs(nums, new List<int>(), result, new bool[nums.Length]);
+        }
+        
+        private void Dfs(int[] input, List<int> temp, List<IList<int>> result, bool[] used)
+        {
+            if (temp.Count == input.Length)
+            {
+                result.Add(new List<int>(temp));    
+            }
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (used[i] || i > 0 && input[i] == input[i-1] && !used[i - 1]) continue;
+
+                temp.Add(input[i]);
+                used[i] = true;
+                Dfs(input, temp, result, used);
+                used[i] = false;
+                temp.RemoveAt(temp.Count - 1);
+            }
+        }
+    }
+}
