38a5e2bf15aece7885147eef4b99c42ee3322f38
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-06-23 22:15:55 +0300

    T77

---
+++ Leetcode/Backtracking/T77.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Backtracking
+{
+    public class T77
+    {
+        [TestCase(4,2)]
+        public void Test(int numbers, int k)
+        {
+            var elements = new List<int>();
+            for (var i = 1; i <= numbers; i++)
+            {
+                elements.Add(i);
+            }
+            
+            var result = new List<List<int>>();
+            Dfs(0, elements, new List<int>(), result, k);
+        }
+        
+        private void Dfs(int start, List<int> input, List<int> temp, List<List<int>> result, int k)
+        {
+            if (temp.Count == k)
+            {
+                result.Add(new List<int>(temp));    
+            }
+
+            for (var i = start; i < input.Count; i++)
+            {
+                temp.Add(input[i]);
+                Dfs(i + 1, input, temp, result, k);
+                temp.RemoveAt(temp.Count - 1);
+            }
+        }
+    }
+}
