36625353af739fd242ce69fc0e0178f9a062f609
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-09-03 08:18:54 +0300

    T121

---
+++ Leetcode/Dynamic/T121.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dynamic
+{
+    public class T121
+    {
+        [TestCase(new [] {7,1,5,3,6,4}, 5)]
+        [TestCase(new [] {7,6,4,3,1}, 0)]
+        public void MaxProfitTest(int[] prices, int benefit)
+        {
+            var result = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    result = Math.Max(result, prices[j] - prices[i]);
+                }
+            }
+
+            result.Should().Be(benefit);
+        }
+    }
+}
