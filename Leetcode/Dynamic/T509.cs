1cac9fddd20ad43404be53f405e9710500b3151f
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-09-04 07:44:05 +0300

    T509

---
+++ Leetcode/Dynamic/T509.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dynamic
+{
+    public class T509
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        public void TestFib(int number, int output)
+        {
+            GetFib(number).Should().Be(output);
+        }
+        
+        int GetFib(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            var previous = 1;
+            var subPrevious = 0;
+
+            var result = previous + subPrevious;
+            for (var i = 2; i <= n; i++)
+            {
+                result = previous + subPrevious;
+                subPrevious = previous;
+                previous = result;
+            }
+
+            return result;
+        }
+    }
+}
