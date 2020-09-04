d9c817f552cd79a1dbb587bc07fc7d2150c15531
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-09-04 07:27:42 +0300

    T70 recursion

---
+++ Leetcode/Dynamic/T70.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dynamic
+{
+    public class T70
+    {
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 5)]
+        [TestCase(5, 8)]
+        [TestCase(6, 13)]
+        public void TestClimbStairs(int n, int output)
+        {
+            var result = GetCountClimbStairs(n);
+
+            result.Should().Be(output);
+        }
+
+        int GetCountClimbStairs(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+
+            return GetCountClimbStairs(n - 1) + GetCountClimbStairs(n - 2);
+        }
+    }
+}
