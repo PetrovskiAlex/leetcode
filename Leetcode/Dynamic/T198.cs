4377f79b512568441bf315f6ae49fb58fc182dc6
Alex Petrovski <petrovski.alex.v@gmail.com>
2020-09-05 21:29:21 +0300

    T198

---
+++ Leetcode/Dynamic/T198.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dynamic
+{
+    public class T198
+    {
+        [TestCase(new [] {1,2,3,1}, 4)]
+        [TestCase(new [] {2,7,9,3,1}, 12)]
+        [TestCase(new [] {2,1,1,2}, 4)]
+        public void TestRob(int[] nums, int output)
+        {
+            var result = 0;
+
+            var even = 0;
+            var odd = 0;
+    
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    even = Math.Max(even + nums[i], odd);
+                }
+                else
+                {
+                    odd = Math.Max(odd + nums[i], even);
+                }
+            }
+
+            result = Math.Max(even, odd);
+            result.Should().Be(output);
+        }
+    }
+}
